271861f2c2d9257fd39963b8302e8bcf4dab2c2e
Nahun Morales <morales.nahun.hn@gmail.com>
2025-08-15 13:32:56 -0600

    SSL para empleo

--- docs/Scripts/Java.js
+++ docs/Scripts/Java.js
@@ -314,7 +314,7 @@ function enviarSolicitudEmpleo(event) {
   
     console.log('Enviando solicitud de empleo:', { nombre, correo, mensaje });
   
-    fetch('http://10.10.20.26:3000/api/contacto', {
+    fetch('https://10.10.20.26:3000/api/contacto', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
